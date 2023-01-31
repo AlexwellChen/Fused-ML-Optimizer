fee5057560632c0bf16e863fab28a205b31196ec
alexwell <cfz1885@gmail.com>
2023-01-31 13:45:24 +0100

    fused_adam kernel

---
+++ fused_adam/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+nvcc_args = ['-maxrregcount=18']
+
+setup(
+    name='fused_adam',
+    ext_modules=[
+        CUDAExtension('fused_adam', [
+            'pybind_adam.cpp',
+            'fused_adam_kernel.cu',
+        ], extra_compile_args={'nvcc': nvcc_args})
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
